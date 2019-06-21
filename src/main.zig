f70a9ea4236273989a7d008d5c4ad7a11e48b7f5
Gray Olson <gray@grayolson.com>
2019-06-21 11:01:08 -0700

    comptime

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,19 @@
 const std = @import("std");
 const c = @import("c.zig");
 
-// pub fn mat4_identity() c.mat4 {
-//     return [4][4]f32 {
-//         {1.0, 0.0, 0.0, 0.0},
-//         {0.0, 1.0, 0.0, 0.0},
-//         {0.0, 0.0, 1.0, 0.0},
-//         {0.0, 0.0, 0.0, 1.0}
-//     };
-// }
+fn mat4_identity() c.mat4 {
+    return [4][4]f32 {
+        [4]f32{1.0, 0.0, 0.0, 0.0},
+        [4]f32{0.0, 1.0, 0.0, 0.0},
+        [4]f32{0.0, 0.0, 1.0, 0.0},
+        [4]f32{0.0, 0.0, 0.0, 1.0}
+    };
+}
 
 pub fn main() anyerror!void {
     const num: f32 = 5.0;
     comptime var translation: c.vec3 = [3]f32{1.0, 0.0, 0.0};
-    comptime var translate_mat: c.mat4 = undefined;
-    c.glm_translate_make(&translate_mat, &translation);
-    std.debug.warn("{}", num);
+    comptime var translate_mat: c.mat4 = mat4_identity();
+    c.glm_translate_make(@ptrCast([*c][4]f32, &translate_mat), @ptrCast([*c]f32, &translation));
+    std.debug.warn("{}", translate_mat);
 }
