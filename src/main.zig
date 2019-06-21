547296ac4a3271a3fad6656c2341154b8b414c34
Gray Olson <gray@grayolson.com>
2019-06-21 10:50:46 -0700

    test commit

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,19 @@
 const std = @import("std");
 const c = @import("c.zig");
 
+// pub fn mat4_identity() c.mat4 {
+//     return [4][4]f32 {
+//         {1.0, 0.0, 0.0, 0.0},
+//         {0.0, 1.0, 0.0, 0.0},
+//         {0.0, 0.0, 1.0, 0.0},
+//         {0.0, 0.0, 0.0, 1.0}
+//     };
+// }
+
 pub fn main() anyerror!void {
     const num: f32 = 5.0;
-    var translation: c.vec3 = f32[4]{1.0, 0.0, 0.0};
-    var translate_mat: c.mat4 = undefined;
-    c.glm_translate_make(translate_mat, translation);
+    comptime var translation: c.vec3 = [3]f32{1.0, 0.0, 0.0};
+    comptime var translate_mat: c.mat4 = undefined;
+    c.glm_translate_make(&translate_mat, &translation);
     std.debug.warn("{}", num);
 }
